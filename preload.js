f68842b2286ccef332ca610d30165112a3043ff7
Filip Luchianenco <filip@Filips-MacBook-Pro.local>
2018-07-02 23:51:20 -0500

    (player):implement youtube player & media controls

---
+++ preload.js
@@ -0,0 +1,12 @@
+// in preload scripts, we have access to node.js and electron APIs
+// the remote web app will not, so this is safe
+const { ipcRenderer: ipc, remote } = require('electron');
+
+
+init();
+
+
+function init() {
+  // left blank for now 
+}
+
